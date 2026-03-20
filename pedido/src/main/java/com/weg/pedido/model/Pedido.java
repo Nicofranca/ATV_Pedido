2c9ff8cf0cf58f5afeeb110d4a92feff4f902996
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-20 13:52:59 -0300

    feat: incia projeto

---
+++ pedido/src/main/java/com/weg/pedido/model/Pedido.java
@@ -0,0 +1,15 @@
+package com.weg.pedido.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Pedido {
+}
---
+++ pedido/src/test/java/com/weg/pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
