17973f6155e43bb383eebf4d15ef5fa6ddd3c028
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-20 18:40:22 -0300

    feat: cria entidades

--- pedido/src/main/java/com/weg/pedido/model/Pedido.java
+++ pedido/src/main/java/com/weg/pedido/model/Pedido.java
@@ -1,15 +1,32 @@
 package com.weg.pedido.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "pedido")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "data_pedido")
+    @CreatedDate
+    private LocalDate dataPedido;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> pedidos;
+
 }
