4caf020a7c92e835b60023a8d14ba2f66e7afb03
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-20 20:39:29 -0300

    feat: cria repositorios

---
+++ pedido/src/main/java/com/weg/pedido/repository/DepartamentoRepositoryJpa.java
@@ -0,0 +1,9 @@
+package com.weg.pedido.repository;
+
+import com.weg.pedido.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepositoryJpa extends JpaRepository<Departamento, Long> {
+}
---
+++ pedido/src/main/java/com/weg/pedido/repository/FuncionarioRepositoryJpa.java
@@ -0,0 +1,15 @@
+package com.weg.pedido.repository;
+
+import com.weg.pedido.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FuncionarioRepositoryJpa extends JpaRepository<Funcionario, Long> {
+    List<Funcionario> findByName(String nome);
+    List<Funcionario> findByDepartamentoId(Long id);
+    Funcionario findByIdAndNome(Long id, String nome);
+
+}
