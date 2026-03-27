78dd667937048c654b54e6391c78177766de8bb0
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-27 19:59:05 -0300

    feat: adiciona status code no controller

--- pedido/src/main/java/com/weg/pedido/controller/PedidoController.java
+++ pedido/src/main/java/com/weg/pedido/controller/PedidoController.java
@@ -4,6 +4,7 @@ import com.weg.pedido.dto.pedido.PedidoRequestDTO;
 import com.weg.pedido.dto.pedido.PedidoResponseDTO;
 import com.weg.pedido.service.PedidoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,31 @@ public class PedidoController {
 
     @PostMapping
     public ResponseEntity<PedidoResponseDTO> save(@RequestBody PedidoRequestDTO pedidoRequestDTO) {
-        return ResponseEntity.ok(pedidoService.save(pedidoRequestDTO));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pedidoService.save(pedidoRequestDTO));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PedidoResponseDTO> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(pedidoService.findById(id));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pedidoService.findById(id));
     }
 
     @GetMapping
     public ResponseEntity<List<PedidoResponseDTO>> findAll() {
-        return ResponseEntity.ok(pedidoService.findAll());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pedidoService.findAll());
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PedidoResponseDTO> update(@PathVariable Long id, @RequestBody PedidoRequestDTO pedidoRequestDTO) {
-        return ResponseEntity.ok(pedidoService.update(id, pedidoRequestDTO));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pedidoService.update(id, pedidoRequestDTO));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<PedidoResponseDTO> delete(@PathVariable Long id) {
-        return ResponseEntity.ok(pedidoService.delete(id));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(pedidoService.delete(id));
     }
 }
